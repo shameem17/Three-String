d5b8086049986d383ce06e70ac3485a879ca63d0
Shameem Ahammed <53037559+shameem17@users.noreply.github.com>
2020-02-15 16:05:34 +0600

    initial commit

---
+++ threestring.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    string a,b,c;
+    while(t--)
+    {
+        cin>>a>>b>>c;
+        int l=a.length();
+        int f=0;
+        for(int i=0;i<l;i++)
+        {
+            if((a[i]==c[i]&&c[i]!=b[i])||(a[i]==c[i]&&c[i]==b[i]))
+            {
+                f++;
+            }
+            else if((c[i]==b[i]&&c[i]!=a[i])||(a[i]==c[i]&&c[i]==b[i]))
+            {
+                f++;
+            }
+            else
+            {
+                cout<<"NO"<<endl;
+                break;
+            }
+            if(f==l)
+            {
+                cout<<"YES"<<endl;
+            }
+
+        }
+    }
+}
